0430df30c8b576a6a28775af5b35ff84a9f44d3d
Marija Bezulj <mejasmi@gmail.com>
2023-04-05 13:49:18 +0200

    Initial commit: sliding window script (framework) full_analysis performs args parsing and calling the method's run and processing functions. community_clustering_algorithm is an abstract class for all community clusterng approaches. It defines some common functions and provides blocks for method specific functions. sliding_window.py provides the sliding window algo for cell mixture extraction, clustering and data saving.

---
+++ notebooks/community_clustering_algorithm.py
@@ -0,0 +1,158 @@
+import logging
+import os
+
+import scanpy as sc
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from anndata import AnnData
+from matplotlib import pyplot as plt
+# import skimage.measure
+# import scipy.ndimage.measurements
+from itertools import cycle
+
+from abc import ABC, abstractmethod
+
+class CommunityClusteringAlgo(ABC):
+    def __init__(self, adata, **params):
+        self.adata = adata
+        self.adata.uns['algo_params'] = params
+        self.adata.uns['sample_name'] = os.path.basename(self.adata.uns['algo_params']['file'].rsplit(".", 1)[0])
+        for key, value in params.items():
+            setattr(self, key, value)
+
+        self.unique_cell_type = self.adata.obs[self.annotation].cat.categories
+        self.tissue = None
+        # [NOTE] this should be included later
+        # # define if each cell type needs a minimum amount of cells to be considered in cell mixtures and what is the minimum value
+        # min_count_per_type_limit = False
+        # min_count_per_type = 100
+
+        # # define if only cell types with more than min_count_per_type cells are used for cell communities extraction
+        # if min_count_per_type_limit:
+
+        #     cell_over_limit = []
+        #     for cell_tp in adata.obs[annotation_label].cat.categories:
+        #         cell_num = sum(adata.obs[annotation_label]==cell_tp)
+        #         if cell_num > min_count_per_type:
+        #             cell_over_limit.append(cell_tp)
+
+        #     adata = adata[adata.obs[annotation_label].isin(cell_over_limit),:]
+
+    @abstractmethod
+    def run(self):
+        pass
+
+    @abstractmethod
+    def save_results(self):
+        pass
+
+    def calc_feature_matrix(self):
+        pass
+
+    def cluster(self):
+        pass
+
+    def plot_clustering(
+        self,
+        sample_name="unknown",
+        color=['clusters'],
+        show=False,
+        palette=None
+        ):
+        # # plot initial clustering for each window
+        # sc.pl.spatial(self.tissue, color='leiden', spot_size=1)
+        # # plot clustering after majority voting for each subwindow
+        # sc.pl.spatial(self.tissue, color='leiden_max_vote', spot_size=1)
+
+        figure, ax = plt.subplots(nrows=1, ncols=1)
+        sc.pl.spatial(self.adata, color=color, palette=palette, spot_size=self.spot_size, ax=ax, show=show)
+        figure.savefig(os.path.join(self.dir_path, sample_name), dpi=300, bbox_inches='tight')
+        plt.close()
+
+    def calculate_spatial_cell_type_metrics(self):
+        pass
+    
+    def calculate_cell_mixture_stats(self):
+
+        # extract information on leiden clustering labels and cell types to create cell communities statistics
+        clustering_labels = f'tissue_{self.method_key}'
+        cell_types_communities = self.adata.obs[[clustering_labels, self.annotation]]
+
+        stats_table = {}
+        # calculate cell type mixtures for every cluster
+        for label, cluster_data in cell_types_communities.groupby(clustering_labels):
+            cell_type_dict = {ct:0 for ct in self.unique_cell_type}
+            for cell in cluster_data[self.annotation]:
+                cell_type_dict[cell]+=1
+            # remove excluded cell types
+            cell_type_dict = {k:cell_type_dict[k] for k in self.tissue.var.index}
+            stats_table[label] = {k:cell_type_dict[k] for k in cell_type_dict}
+
+            stats_table[label]['total_counts'] = int(sum(cell_type_dict.values()))
+
+        stats = pd.DataFrame(stats_table).T
+        # stats.index.name='clusters'
+        stats.columns.name="cell types"
+
+        # add final row with total counts per cell types
+        cell_type_counts = {ct:[int(sum(stats[ct].values))] for ct in self.tissue.var.index}
+        stats = pd.concat([stats, pd.DataFrame(cell_type_counts, index=['total_cells'])])
+
+        # divide each row with total sum of cells per cluster
+        for i in range(len(stats.index.values[:-1])):
+            if stats.iloc[i,-1] > 0:
+                stats.iloc[i, :-1] = (100 * stats.iloc[i, :-1] / stats.iloc[i, -1]).astype(int)
+        # save cell mixture statistics to csv file and to tissue
+        self.tissue.uns['cell mixtures'] = stats.iloc[:, :]
+
+    def plot_stats(self):
+        stats = self.tissue.uns['cell mixtures']
+        sc.settings.set_figure_params(dpi=400, facecolor='white')
+        sns.set(font_scale=0.2)
+
+        ncols = len(stats.columns) # we want to separately print the total_counts column
+        fig, axes = plt.subplots(ncols=ncols)
+
+        # no space between columns
+        fig.subplots_adjust(wspace=0)
+
+        # put colormaps of your choice in a list:
+        cmap_cycle = cycle(['Greens', 'Reds', 'Blues', 'Oranges', 'Purples'])
+
+        for i, ax in enumerate(axes):
+            g = sns.heatmap(pd.DataFrame(stats.iloc[:, i]), vmin=0.0, vmax=50, linewidths=0, linecolor=None, annot=True, cbar=False, ax=ax, cmap=cmap_cycle.__next__(),fmt='4.0f', xticklabels=True, yticklabels=True if i==0 else False, square=True)
+            g.set_xticklabels(g.get_xticklabels(), rotation=70)
+            g.xaxis.tick_top() # x axis on top
+        # final column should have the sum of all cells per cluster
+        sns.heatmap(pd.DataFrame(stats.iloc[:, -1]), annot=True, linewidths=0, linecolor=None, cbar=False, cmap=None, ax=ax, fmt='4.0f', xticklabels=True, yticklabels=False, square=True)
+        plt.savefig(os.path.join(self.dir_path, f'cell_mixture_table_{self.params_suffix}.png'), dpi=400)
+        plt.close()
+
+        min_cell_types = 2
+        min_perc = 20
+        min_perc_to_show = 5
+        min_cells_in_cluster = 500
+
+        sc.settings.set_figure_params(dpi=100, facecolor='white')
+
+        new_stats = stats.copy()
+        new_stats = new_stats.drop(labels='total_counts', axis=1)
+        new_stats = new_stats.drop(labels='total_cells', axis=0)
+        for cluster in new_stats.iterrows():
+            ct_perc = cluster[1].sort_values(ascending=False)
+            if ct_perc[min_cell_types-1] > min_perc and stats.loc[cluster[0]]['total_counts']>min_cells_in_cluster:
+                ct_ind = [x for x in ct_perc.index[ct_perc>min_perc_to_show]]
+                
+                fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(20,8))
+
+                sc.pl.spatial(self.adata, groups=ct_ind, color=self.annotation, spot_size=self.spot_size, ax=ax[0], show=False)
+                ax[0].legend([f'{ind} ({ct_perc[ind]})' for ind in ct_ind], bbox_to_anchor=(1.0, 0.5), loc='center left', frameon=False, fontsize=12)
+
+                
+                sc.pl.spatial(self.adata, groups=[cluster[0]], color=f'tissue_{self.method_key}', spot_size=self.spot_size, ax=ax[1], show=False)
+                fig.subplots_adjust(wspace=0.35)
+
+                fig.savefig(os.path.join(self.dir_path, f'cmixtures_{self.params_suffix}_c{cluster[0]}.png'), bbox_inches='tight')
+
+                plt.close()
---
+++ notebooks/sliding_window.py
@@ -0,0 +1,166 @@
+
+import logging
+import os
+
+import scanpy as sc
+import numpy as np
+import pandas as pd
+
+from anndata import AnnData
+from matplotlib import pyplot as plt
+import skimage.measure
+import scipy.ndimage.measurements
+
+from community_clustering_algorithm import CommunityClusteringAlgo
+
+class SlidingWindow(CommunityClusteringAlgo):
+    def __init__(self, adata, **params):
+        super().__init__(adata, **params)
+        self.params_suffix = f"_sldwin_r{self.resolution}_ws{self.win_size}_ss{self.sliding_step}_entt{self.entropy_thres}_scatt{self.scatter_thres}_dwnsr{self.downsample_rate}"
+        self.filename = self.adata.uns['sample_name'] + self.params_suffix
+        self.dir_path = os.path.join(self.adata.uns['algo_params']['out_path'], self.filename)
+        # create results folder
+        if (not os.path.exists(self.dir_path)):
+            os.mkdir(self.dir_path)
+
+        self.method_key = 'sliding_window'
+       
+
+    def calc_feature_matrix(self):
+        # bin size needs to be a multiple of sliding step
+        self.sliding_step = (self.win_size/int((self.win_size/self.sliding_step))) if self.sliding_step!=None else self.win_size
+        bin_slide_ratio = int(self.win_size/self.sliding_step)
+
+        # create centroids for each sliding step of windows
+        self.adata.obs['Centroid_X'] = ((self.adata.obsm['spatial'][:,0])/self.sliding_step).astype(int)
+        self.adata.obs['Centroid_Y'] = ((self.adata.obsm['spatial'][:,1])/self.sliding_step).astype(int)
+        # need to understand borders and padding
+        # subwindows belonging to borders will not have a complete cell count
+        x_max = self.adata.obs['Centroid_X'].max()
+        y_max = self.adata.obs['Centroid_Y'].max()
+
+
+        self.adata.obs['x_y'] = self.adata.obs['Centroid_X'].astype(str) +'_'+self.adata.obs['Centroid_Y'].astype(str)
+        tmp = self.adata.obs[['x_y', self.annotation]]
+        
+        ret = {}
+        # calculate features for each subwindow
+        for sw_ind, sw_data in tmp.groupby('x_y'):
+            templete_dic = {ct:0 for ct in self.unique_cell_type}
+            for cell in sw_data[self.annotation]:
+                templete_dic[cell]+=1
+            ret[sw_ind] = templete_dic
+        # merge features by windows
+        feature_matrix = {}
+        for subwindow in ret.keys():
+            # index of window is in the top left corner of the whole window
+            feature_matrix[subwindow] = {}
+            x_curr = int(subwindow.split("_")[0])
+            y_curr = int(subwindow.split("_")[1])
+            # # count the number of subwindows participating in full window feature vec
+            # num_subw = 0
+
+            for slide_x in range(0, np.min([bin_slide_ratio, x_max-x_curr+1])):
+                for slide_y in range(0, np.min([bin_slide_ratio, y_max-y_curr+1])):  # starts from 1 since values with coordinates (0,0) are already written by initializing with ret[subwindow]
+                    if (f'{x_curr + slide_x}_{y_curr + slide_y}') in ret.keys():
+                        # num_subw += 1
+                        feature_matrix[subwindow] = {k: 
+                                                    feature_matrix[subwindow].get(k, 0) + ret[f'{x_curr + slide_x}_{y_curr + slide_y}'].get(k, 0)
+                                                    for k in set(feature_matrix[subwindow]).union(ret[f'{x_curr + slide_x}_{y_curr + slide_y}'])}
+            # # scale the feature values by the number of summed subwindows that form it (it could be useful as feature vector normalization)
+            # feature_matrix[subwindow] = {k:feature_matrix[subwindow][k]/num_subw for k in feature_matrix[subwindow].keys()}
+            
+            # scale the feature vector by the total numer of cells in it
+            norm_factor = self.total_cell_norm/sum(feature_matrix[subwindow].values())
+            for k in feature_matrix[subwindow]:
+                feature_matrix[subwindow][k] = int(feature_matrix[subwindow][k] * norm_factor)
+                
+        feature_matrix = pd.DataFrame(feature_matrix).T
+        self.tissue = AnnData(feature_matrix, dtype=np.int64)
+        self.tissue.obsm['spatial'] = np.array([[x.split('_')[0], x.split('_')[1]] for x in feature_matrix.index]).astype(int)
+
+    def calculate_spatial_cell_type_metrics(self):
+        # calculate cell type specific global metrics
+        # [NOTE] this could go into a function of the class
+        # entropy & scatteredness
+        self.adata.obs['x_coor'] = (self.adata.obsm['spatial'][:,0])
+        self.adata.obs['y_coor'] = (self.adata.obsm['spatial'][:,1])
+        cx_min = np.min(self.adata.obs['x_coor'])
+        cx_max = np.max(self.adata.obs['x_coor'])
+        cy_min = np.min(self.adata.obs['y_coor'])
+        cy_max = np.max(self.adata.obs['y_coor'])
+
+        self.tissue.var['entropy'] = pd.Series(index=self.unique_cell_type, name='entropy', dtype=np.float64)
+        self.tissue.var['scatteredness'] = pd.Series(index=self.unique_cell_type, name='scatteredness', dtype=np.float64)
+        for cell_t in self.unique_cell_type:
+            tissue_window = np.zeros(shape=(int(np.ceil((cx_max-cx_min+1)/self.downsample_rate)), int(np.ceil((cy_max-cy_min+1)/self.downsample_rate))), dtype=np.int8)
+            tissue_window[((self.adata.obs['x_coor'][self.adata.obs[self.annotation] == cell_t] - cx_min)/self.downsample_rate).astype(int), ((self.adata.obs['y_coor'][self.adata.obs[self.annotation] == cell_t] - cy_min)/self.downsample_rate).astype(int)] = 1
+            self.tissue.uns[f'tw_{cell_t}'] = tissue_window
+            
+            self.tissue.var['entropy'].loc[cell_t] = skimage.measure.shannon_entropy(tissue_window)
+            _, num_objects = scipy.ndimage.measurements.label(tissue_window, structure=None, output=None) # this assumes 4 neighbors connectivity
+            # # idea for scatteredness was to compute the number of connected components and divide it with number of existing non-zero elements
+            # # but this measure does not contain the information on percentage of non-zero elements in the matrix.
+            # # thus we multiply it with non-zero percentage (num non-zero / total num) creating just this formula
+            # # num_object/image.size
+            # # max value is based on neighbors size (if 4 then 1/4, if 8, 1/8), min value is 0 if there are no non-zero elements
+            self.tissue.var['scatteredness'].loc[cell_t] = num_objects/tissue_window.size *4
+
+    def run(self):
+        if self.tissue==None:
+            self.calc_feature_matrix()
+        
+            self.calculate_spatial_cell_type_metrics()
+
+            var_use = self.tissue.var.loc[(self.tissue.var['entropy']<self.entropy_thres) & (self.tissue.var['scatteredness']<self.scatter_thres)].index
+            self.tissue.raw = self.tissue
+            self.tissue = self.tissue[:, var_use]
+        if f'tissue_{self.method_key}' not in self.adata.obs.keys():
+            self.cluster()
+
+    def cluster(self):
+
+        sc.pp.neighbors(self.tissue, use_rep='X')
+        sc.tl.leiden(self.tissue, resolution=self.resolution)
+
+        bin_slide_ratio = int(self.win_size/self.sliding_step)
+        x_min = self.adata.obs['Centroid_X'].min()
+        y_min = self.adata.obs['Centroid_Y'].min()
+        # max voting on cluster labels
+        # init the new obs column
+        self.tissue.obs['leiden_max_vote'] = list('x' for x in range(len(self.tissue.obs.index)))
+        for x_curr, y_curr in self.tissue.obsm['spatial']:
+            # index of subwindow is in the top left corner of the whole window
+            subwindow_labels = {}
+            for slide_x in range(0, np.min([bin_slide_ratio, x_curr - x_min + 1])):
+                for slide_y in range(0, np.min([bin_slide_ratio, y_curr - y_min + 1])):
+                    # check if location exist (spatial area is not complete)
+                    if (f'{x_curr - slide_x}_{y_curr - slide_y}') in self.tissue.obs.index:
+                        new_value = self.tissue.obs.loc[f'{x_curr - slide_x}_{y_curr - slide_y}', 'leiden']
+                        subwindow_labels[new_value] = subwindow_labels[new_value] + 1 if new_value in subwindow_labels.keys() else 1
+            
+            # max vote
+            # max vote should be saved in a new obs column so that it does not have diagonal effect on
+            # other labels during refinment
+            self.tissue.obs['leiden_max_vote'][f'{x_curr}_{y_curr}'] = max(subwindow_labels, key=subwindow_labels.get)
+
+        self.adata.obs[f'tissue_{self.method_key}'] = list(self.tissue.obs.loc[self.adata.obs['x_y'], 'leiden_max_vote'])
+
+        logging.info(r"Sliding window cell mixture calculation done. Added results to adata.obs['sliding_window']")
+
+        
+    def save_results(self):
+        
+        for cell_t in self.unique_cell_type:
+            plt.imsave(fname=os.path.join(self.dir_path, f'tissue_window_{cell_t}_{self.params_suffix}.png'), arr=self.tissue.uns[f'tw_{cell_t}'], vmin=0, vmax=1, cmap='gray', dpi=250)
+        
+        # save metrics results in csv format
+        # print(self.tissue.var[['entropy', 'scatteredness']])
+        self.tissue.var[['entropy', 'scatteredness']].to_csv(os.path.join(self.dir_path, f'spatial_metrics_{self.params_suffix}.csv'))
+
+        # save anndata file
+        self.tissue.write_h5ad(os.path.join(self.dir_path, f'tissue_{self.filename}.h5ad'), compression="gzip")
+
+        logging.info(f'Saved clustering result tissue_{self.filename}.h5ad.')
+
+        self.tissue.uns['cell mixtures'].to_csv(os.path.join(self.dir_path, f'cell_mixture_stats_{self.params_suffix}.csv'))
