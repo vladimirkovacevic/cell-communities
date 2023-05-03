38b8cc4f03d49c14ceb6743719c2e478dc6100d4
josicb16 <josicbojana16@gmail.com>
2023-05-03 13:10:59 +0000

    calculate feature matrix with multiple win_sizes

--- core/sliding_window.py
+++ core/sliding_window.py
@@ -32,61 +32,65 @@ class SlidingWindow(CommunityClusteringAlgo):
             else:
                 raise AttributeError(f"File '{self.tfile}' extension is not .h5ad")
 
-
     def calc_feature_matrix(self):
-        win_size = self.win_sizes_list[0]
-        sliding_step = self.sliding_steps_list[0]
+        whole_feature_matrix = pd.DataFrame()
 
-        # window size needs to be a multiple of sliding step
-        sliding_step = (win_size/int((win_size/sliding_step))) if sliding_step!=None else win_size
-        bin_slide_ratio = int(win_size/sliding_step)
+        for win_size, sliding_step in zip(self.win_sizes_list, self.sliding_steps_list):
+            # window size needs to be a multiple of sliding step
+            sliding_step = (win_size/int((win_size/sliding_step))) if sliding_step!=None else win_size
+            bin_slide_ratio = int(win_size/sliding_step)
 
-        # create centroids for each sliding step of windows
-        # .obs data is assigned as pd.Series since sometimes the new column added to .obs Dataframe can have 'nan' values
-        # if the index of data doesn't match the index of .obs
-        self.adata.obs['Centroid_X'] = pd.Series(((self.adata.obsm['spatial'][:,0])/sliding_step).astype(int), index=self.adata.obs_names)
-        self.adata.obs['Centroid_Y'] = pd.Series(((self.adata.obsm['spatial'][:,1])/sliding_step).astype(int), index=self.adata.obs_names)
-        # need to understand borders and padding
-        # subwindows belonging to borders will not have a complete cell count
-        x_max = self.adata.obs['Centroid_X'].max()
-        y_max = self.adata.obs['Centroid_Y'].max()
+            # create centroids for each sliding step of windows
+            # .obs data is assigned as pd.Series since sometimes the new column added to .obs Dataframe can have 'nan' values
+            # if the index of data doesn't match the index of .obs
+            self.adata.obs['Centroid_X'] = pd.Series(((self.adata.obsm['spatial'][:,0])/sliding_step).astype(int), index=self.adata.obs_names)
+            self.adata.obs['Centroid_Y'] = pd.Series(((self.adata.obsm['spatial'][:,1])/sliding_step).astype(int), index=self.adata.obs_names)
+            # need to understand borders and padding
+            # subwindows belonging to borders will not have a complete cell count
+            x_max = self.adata.obs['Centroid_X'].max()
+            y_max = self.adata.obs['Centroid_Y'].max()
 
+            self.adata.obs['x_y'] = self.adata.obs['Centroid_X'].astype(str) +'_'+self.adata.obs['Centroid_Y'].astype(str)
+            
+            tmp = self.adata.obs[['x_y', self.annotation]]
+            ret = {}
+            # calculate features for each subwindow
+            for sw_ind, sw_data in tmp.groupby('x_y'):
+                templete_dic = {ct:0 for ct in self.unique_cell_type}
+                for cell in sw_data[self.annotation]:
+                    templete_dic[cell]+=1
+                ret[sw_ind] = templete_dic
+            # merge features by windows
+            feature_matrix = {}
+            for subwindow in ret.keys():
+                # index of window is in the top left corner of the whole window
+                subwindow_id = str(subwindow) + '_' + str(win_size) 
+                feature_matrix[subwindow_id] = {}
+                x_curr = int(subwindow.split("_")[0])
+                y_curr = int(subwindow.split("_")[1])
+                # # count the number of subwindows participating in full window feature vec
+                # num_subw = 0
 
-        self.adata.obs['x_y'] = self.adata.obs['Centroid_X'].astype(str) +'_'+self.adata.obs['Centroid_Y'].astype(str)
+                for slide_x in range(0, np.min([bin_slide_ratio, x_max-x_curr+1])):
+                    for slide_y in range(0, np.min([bin_slide_ratio, y_max-y_curr+1])):  # starts from 1 since values with coordinates (0,0) are already written by initializing with ret[subwindow]
+                        if (f'{x_curr + slide_x}_{y_curr + slide_y}') in ret.keys():
+                            # num_subw += 1
+                            feature_matrix[subwindow_id] = {k: 
+                                                        feature_matrix[subwindow_id].get(k, 0) + ret[f'{x_curr + slide_x}_{y_curr + slide_y}'].get(k, 0)
+                                                        for k in set(feature_matrix[subwindow_id]).union(ret[f'{x_curr + slide_x}_{y_curr + slide_y}'])}
+
+                    
+            feature_matrix = pd.DataFrame(feature_matrix).T
+            pd.concat([whole_feature_matrix, feature_matrix], axis = 0)
         
-        tmp = self.adata.obs[['x_y', self.annotation]]
-        ret = {}
-        # calculate features for each subwindow
-        for sw_ind, sw_data in tmp.groupby('x_y'):
-            templete_dic = {ct:0 for ct in self.unique_cell_type}
-            for cell in sw_data[self.annotation]:
-                templete_dic[cell]+=1
-            ret[sw_ind] = templete_dic
-        # merge features by windows
-        feature_matrix = {}
-        for subwindow in ret.keys():
-            # index of window is in the top left corner of the whole window
-            feature_matrix[subwindow] = {}
-            x_curr = int(subwindow.split("_")[0])
-            y_curr = int(subwindow.split("_")[1])
-            # # count the number of subwindows participating in full window feature vec
-            # num_subw = 0
 
-            for slide_x in range(0, np.min([bin_slide_ratio, x_max-x_curr+1])):
-                for slide_y in range(0, np.min([bin_slide_ratio, y_max-y_curr+1])):  # starts from 1 since values with coordinates (0,0) are already written by initializing with ret[subwindow]
-                    if (f'{x_curr + slide_x}_{y_curr + slide_y}') in ret.keys():
-                        # num_subw += 1
-                        feature_matrix[subwindow] = {k: 
-                                                    feature_matrix[subwindow].get(k, 0) + ret[f'{x_curr + slide_x}_{y_curr + slide_y}'].get(k, 0)
-                                                    for k in set(feature_matrix[subwindow]).union(ret[f'{x_curr + slide_x}_{y_curr + slide_y}'])}
-
-                
-        feature_matrix = pd.DataFrame(feature_matrix).T
         # feature_matrix is placd in AnnData object with specified spatial cooridnated of the sliding windows
-        self.tissue = AnnData(feature_matrix.astype(np.float32), dtype=np.float32)
+        self.tissue = AnnData(whole_feature_matrix.astype(np.float32), dtype=np.float32)
         # spatial coordinates are expanded with 3rd dimension with slice_id 
         # this should enable calculation of multislice cell communities
-        self.tissue.obsm['spatial'] = np.array([[x.split('_')[0], x.split('_')[1], self.slice_id] for x in feature_matrix.index]).astype(int)
+        self.tissue.obsm['spatial'] = np.array([[x.split('_')[0], x.split('_')[1], self.slice_id] for x in whole_feature_matrix.index]).astype(int)
+        self.tissue.obsm['window_size'] = np.array([x.split('_')[2] for x in whole_feature_matrix.index]).astype(int)
+        
         self.tissue.obs = self.tissue.obs.copy()
         self.tissue.obs['window_cell_sum'] = np.sum(self.tissue.X, axis=1)
         # remove feature vectors which have less than a specified amount of time
