a69cb6082a61b0e074a5df99f81f5925de264bd6
Vladimir Kovacevic <vladimirkovacevic@genomics.cn>
2023-05-31 13:45:19 +0000

    [CLUSTERING] Spectral clustering added

--- ccd/sliding_window.py
+++ ccd/sliding_window.py
@@ -43,7 +43,7 @@ class SlidingWindow(CommunityClusteringAlgo):
         assert len(self.win_sizes_list) == len(self.sliding_steps_list), \
             "The number of sliding steps must be equal to the number of window sizes."
         win_sizes = "_".join([str(i) for i in self.win_sizes_list])
-        self.params_suffix = f"_sldwin_sl{self.slice_id}_r{self.resolution}_ws{win_sizes}_en{self.entropy_thres}_sct{self.scatter_thres}_dwr{self.downsample_rate}_mcc{self.min_cells_coeff}"
+        self.params_suffix = f"_sldwin_sl{self.slice_id}_c{self.cluster_algo}_r{self.resolution}_ws{win_sizes}_en{self.entropy_thres}_sct{self.scatter_thres}_dwr{self.downsample_rate}_mcc{self.min_cells_coeff}"
         self.filename = self.adata.uns['sample_name']
         self.dir_path = os.path.join(self.adata.uns['algo_params']['out_path'], self.filename)
         # create results folder
